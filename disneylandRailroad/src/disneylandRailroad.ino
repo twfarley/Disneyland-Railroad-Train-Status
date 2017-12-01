00f924a79434d18a532304c1bd35940bdd80a6bd
twfarley <twfarley@gmail.com>
2017-11-30 18:26:28 -0800

    DEfined months & colors Added if statements for months

--- disneylandRailroad/src/disneylandRailroad.ino
+++ disneylandRailroad/src/disneylandRailroad.ino
@@ -10,7 +10,7 @@
 #define PIXEL_PIN D6
 #define PIXEL_TYPE WS2812B
 
-#define TRAIN_COLOR 255,220,94
+#define TRAIN_ALTCOLOR 255,220,94
 #define TRAIN_OFF_COLOR 0,0,0
 #define TRAIN_LENGTH 8
 
@@ -19,9 +19,26 @@
 #define TOONTOWN 108
 #define TOMORROWLAND 144
 
+#define JAN 173,204,255 //light blue
+#define FEB 255,160,226 //pink
+#define MAR 44,237,37 //clover green
+#define APR 0,255,174 //teal
+#define MAY 255,0,255 //magenta
+#define JUN 0,255,100 //light green
+#define JUL 0,255,0 //green
+#define AUG 255,200,0 //yellow
+#define SEP 255,180,0 //light orange
+#define OCT 255,110,0 //orange
+#define NOV 150,0,255 //purple
+#define DEC 255,0,0 //red
+
 Adafruit_NeoPixel strip(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
 
-int waitTime = Time.second() + 12.5;
+int WAITTIME = Time.second() + 12.5;
+
+uint32_t TRAIN_COLOR = strip.Color(255, 255, 255);
+
+int CURRENT_MONTH = Time.month();
 
 int TRAIN1_POSITION = MAINST;
 int TRAIN1_OFF = TRAIN1_POSITION - TRAIN_LENGTH;
@@ -42,7 +59,7 @@ void setup() {
   Time.zone(-8);
   int SPARKLE_OFF_TIME = (Time.second() + 3);
   while (Time.second() < SPARKLE_OFF_TIME) {
-    SPARKLE(0,0,0, 0);
+    SPARKLE(0,0,0,0);
   }
 }
 
@@ -53,13 +70,50 @@ void loop() {
 }
 
 void TRAIN_CYCLE(){
+  if (CURRENT_MONTH == 1) {
+    TRAIN_COLOR = JAN;
+  } //if CURRENT_MONTH doesn't work then just use Time.month() here.
+  if (CURRENT_MONTH == 2) {
+    TRAIN_COLOR = FEB;
+  }
+  if (CURRENT_MONTH == 3) {
+    TRAIN_COLOR = MAR;
+  }
+  if (CURRENT_MONTH == 4) {
+    TRAIN_COLOR = APR;
+  }
+  if (CURRENT_MONTH == 5) {
+    TRAIN_COLOR = MAY;
+  }
+  if (CURRENT_MONTH == 6) {
+    TRAIN_COLOR = JUN;
+  }
+  if (CURRENT_MONTH == 7) {
+    TRAIN_COLOR = JUL;
+  }
+  if (CURRENT_MONTH == 8) {
+    TRAIN_COLOR = AUG;
+  }
+  if (CURRENT_MONTH == 9) {
+    TRAIN_COLOR = SEP;
+  }
+  if (CURRENT_MONTH == 10) {
+    TRAIN_COLOR = OCT;
+  }
+  if (CURRENT_MONTH == 11) {
+    TRAIN_COLOR = NOV;
+  }
+  if (CURRENT_MONTH == 12) {
+    TRAIN_COLOR = DEC;
+  }
+
   strip.setPixelColor(TRAIN1_POSITION, TRAIN_COLOR);
   strip.setPixelColor(TRAIN1_OFF, TRAIN_OFF_COLOR);
-  strip.setPixelColor(TRAIN2_POSITION, TRAIN_COLOR);
+  strip.setPixelColor(TRAIN2_POSITION, TRAIN_ALTCOLOR);
   strip.setPixelColor(TRAIN2_OFF, TRAIN_OFF_COLOR);
   strip.setPixelColor(TRAIN3_POSITION, TRAIN_COLOR);
   strip.setPixelColor(TRAIN3_OFF, TRAIN_OFF_COLOR);
-  strip.setPixelColor(TRAIN4_POSITION, TRAIN_COLOR);
+  strip.setPixelColor(TRAIN4_POSITION, TRAIN_ALTCOLOR);
   strip.setPixelColor(TRAIN4_OFF, TRAIN_OFF_COLOR);
   strip.setBrightness(1);
   strip.show();
@@ -74,30 +128,30 @@ void TRAIN_CYCLE(){
   TRAIN4_OFF ++;
   strip.show();
 
-  if (TRAIN1_POSITION > PIXEL_COUNT) {
+  if (TRAIN1_POSITION > PIXEL_COUNT - 1) {
     TRAIN1_POSITION = 0;
   }
-  if (TRAIN1_OFF > PIXEL_COUNT) {
+  if (TRAIN1_OFF > PIXEL_COUNT - 1) {
     TRAIN1_OFF = 0;
   }
-  if (TRAIN2_POSITION > PIXEL_COUNT) {
+  if (TRAIN2_POSITION > PIXEL_COUNT - 1) {
     TRAIN2_POSITION = 0;
   }
-  if (TRAIN2_OFF > PIXEL_COUNT) {
+  if (TRAIN2_OFF > PIXEL_COUNT - 1) {
     TRAIN2_OFF = 0;
   }
-  if (TRAIN3_POSITION > PIXEL_COUNT) {
+  if (TRAIN3_POSITION > PIXEL_COUNT - 1) {
     TRAIN3_POSITION = 0;
   }
-  if (TRAIN3_OFF > PIXEL_COUNT) {
+  if (TRAIN3_OFF > PIXEL_COUNT - 1) {
     TRAIN3_OFF = 0;
   }
-  if (TRAIN4_OFF > PIXEL_COUNT) {
+  if (TRAIN4_OFF > PIXEL_COUNT - 1) {
     TRAIN4_OFF = 0;
   }
   strip.show();
 
-  delay(waitTime);
+  delay(WAITTIME);
 }
 
 void SPARKLE(byte red, byte green, byte blue, int SpeedDelay) {
@@ -106,19 +160,4 @@ void SPARKLE(byte red, byte green, byte blue, int SpeedDelay) {
   strip.show();
   delay(SpeedDelay);
   strip.setPixelColor(SPARKLE_PIXEL,0,0,0);
-}
-
-String MONTH_COLOR{
-  uint32_t January = strip.Color(173,204,255); //light blue
-  uint32_t February = strip.Color(255,0,0); //red
-  uint32_t March = strip.Color(44,237,37); //clover green
-  uint32_t April = strip.Color(0,255,174); //teal
-  uint32_t May = strip.Color(255,0,255); //magenta
-  uint32_t June = strip.Color(0,255,100); //light green
-  uint32_t July = strip.Color(0,255,0); //green
-  uint32_t August = strip.Color(255,200,0); //yellow
-  uint32_t September = strip.Color(255,180,0); //light orange
-  uint32_t October = strip.Color(255,110,0); //orange
-  uint32_t November = strip.Color(150,0,255); //purple
-  uint32_t December = strip.Color(0,0,255); //blue
 }
